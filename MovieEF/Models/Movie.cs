61b93038b9e5e6ed604fa8c1f18e33c538dd49d1
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-25 20:10:54 -0400

    Added Schema; about to add and apply migrations

---
+++ MovieEF/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MovieEF.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime? YearReleased { get; set; }
+        public string Tagline { get; set; } = "";
+        public int Rating { get; set; }
+        public string Genre { get; set; }
+    }
+}
---
+++ MovieEF/MovieContext/MoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MovieEF.Models;
+
+namespace MovieEF.MovieContext
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(): base("name=DefaultConnection")
+        {
+        }
+        
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
