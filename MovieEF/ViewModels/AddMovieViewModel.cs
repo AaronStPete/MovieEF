10922dfd89b0a859fddf685f7fef03059b9b4a5a
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-25 20:33:14 -0400

    working on post addmovie() => added AddMovieViewModel

---
+++ MovieEF/ViewModels/AddMovieViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieEF.ViewModels
+{
+    public class AddMovieViewModel
+    {
+        public string Title { get; set; }
+        public DateTime? YearReleased { get; set; }
+        public string Tagline { get; set; } = "";
+        public int? Rating { get; set; }
+        public string Genre { get; set; }
+
+    }
+}
